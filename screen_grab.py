382852be00482d3fe551f99b5fc1b600ec8db854
Alexander Waters <contact@alexwaters.dev>
2023-03-10 20:08:53 -0600

    cleaned

--- screen_grab.py
+++ screen_grab.py
@@ -1,42 +1,36 @@
 import win32gui
 import win32ui
 import win32con
-import win32api
-import numpy as np, cv2
-from timer import timer
-from datetime import timedelta, datetime
+import numpy, cv2
+from PIL import Image
 
-def take_screen_shot(region=None):
+def convert(bitmap) -> cv2.Mat:
+    bmpinfo = bitmap.GetInfo()
+    bmpbits = bitmap.GetBitmapBits(True)
+    pil_im = Image.frombuffer('RGB', (bmpinfo['bmWidth'], bmpinfo['bmHeight']), bmpbits, 'raw', 'BGRX', 0, 1)
+    pil_array = numpy.array(pil_im)    
+    return cv2.cvtColor(pil_array, cv2.COLOR_RGB2BGR)
+
+
+def take_screen_shot():
     # https://stackoverflow.com/questions/3586046/fastest-way-to-take-a-screenshot-with-python-on-windows
-    hwin = win32gui.GetDesktopWindow()
-
-    if region:
-            left,top,x2,y2 = region
-            width = x2 - left + 1
-            height = y2 - top + 1
-    else:
-        width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
-        height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN)
-        left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
-        top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
-
-    hwindc = win32gui.GetWindowDC(hwin)
-    srcdc = win32ui.CreateDCFromHandle(hwindc)
-    memdc = srcdc.CreateCompatibleDC()
-    bmp = win32ui.CreateBitmap()
-    bmp.CreateCompatibleBitmap(srcdc, width, height)
-    memdc.SelectObject(bmp)
-    memdc.BitBlt((0, 0), (width, height), srcdc, (left, top), win32con.SRCCOPY)
+    w = 1920 # set this
+    h = 1080 # set this
     
-    signedIntsArray = bmp.GetBitmapBits(True)
-    img = np.fromstring(signedIntsArray, dtype='uint8')
-    img.shape = (height,width,4)
+    hwnd = win32gui.FindWindow(None, "co-op games.txt - Notepad")
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj=win32ui.CreateDCFromHandle(wDC)
+    cDC=dcObj.CreateCompatibleDC()
+    dataBitMap = win32ui.CreateBitmap()
+    dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+    cDC.SelectObject(dataBitMap)
+    cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
+    
+    image = convert(dataBitMap)
 
-    srcdc.DeleteDC()
-    memdc.DeleteDC()
-    win32gui.ReleaseDC(hwin, hwindc)
-    win32gui.DeleteObject(bmp.GetHandle())
-
-    return cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
-
-
+    # Free Resources
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, wDC)
+    win32gui.DeleteObject(dataBitMap.GetHandle())
+    return image
